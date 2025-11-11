0b7f9425333a9d2f65f4c27690a016e8714df35f
rachel-sedibridge <rachelbridge.sedivy@gmail.com>
2025-11-11 14:08:31 -0500

    some progress made last night -- ish -- but still does not work

--- scanning/scanning_interactive_tone.js
+++ scanning/scanning_interactive_tone.js
@@ -34,27 +34,24 @@ var regions_to_play = {
   ground: [ground_real_ping, true]
 }
 
-// ======================== PLAYER ========================
-var mainPlayers = new Tone.Players().toDestination()
 var startPlayer = new Tone.Player(start_ping).toDestination()
 var stopPlayer = new Tone.Player(stop_ping).toDestination()
 
+startPlayer.sync().start(0)
+
 // TODO: accomodate the "all/selected" toggle, call updateRegions (once it's updated... ha)
-// region:str, attrs:list -> [ping url:str, ischecked:bool]
+// init region players
 for (const [region, attrs] of Object.entries(regions_to_play)) {
   if (attrs[1]) {
-    var newPlayer = new Tone.Player(url = attrs[0])
-    newPlayer.name = region
-    // sync all players to start at time 1 (allow 1 sec for start_ping)
-    newPlayer.sync().start(1)
-    mainPlayers.add(newPlayer)
-    // mainPlayer.add(region, attrs[0]) //old way w/o separately creating new play
+    const channel = new Tone.Channel().toDestination();
+    const player = new Tone.Player({
+      url: `audio_tracks/${attrs[0]}.mp3`,
+      name: region,
+    }).sync();
+    player.onstop = (() => stopPlayer.start())
+    player.connect(channel);
   }
 }
-mainPlayers.onstop = (() => stopPlayer.play())
-// also chain this main one to the start ping player
-startPlayer.onstop = (() => mainPlayers.play())
-
 
 // called before playing anything, every time
 function updateMainPlayer() {
@@ -65,7 +62,6 @@ function updateMainPlayer() {
     }
   }
 }
-// ========================================================
 
 // // TODO: change to accomodate the "all/selected" toggle
 // function updateRegionsToPlay() {
@@ -76,13 +72,6 @@ function updateMainPlayer() {
 //     ground: [ground_real_ping, ground_checkbox.checked]
 //   }
 // }
-
-
-// ======================= SAMPLER ========================
-
-// ========================================================
-
-
 
 // from https://javascript.info/keyboard-events
 
@@ -98,12 +87,14 @@ function handleDown(e) {
     if (e.repeat) {
       console.log('keydown up repeating')
     } else {
+      startPlayer.onstop(setupForwardPlay())
       console.log('keydown up')
     }
   } else if (e.key == triggerDown) {
     if (e.repeat) {
       console.log('keydown down repeating')
     } else {
+      startPlayer.onstop(setupBackwardPlay())
       console.log('keydown down')
     }
   }
@@ -122,6 +113,18 @@ function handleDown(e) {
 
 function handleUp(e) {
   console.log('keyup')
+}
+
+function setupForwardPlay(repeating) {
+  for (const [region, attrs] of Object.entries(regions_to_play)) {
+    if (attrs[1]) {
+      mainPlayers.add(region, attrs[0])
+    }
+  }
+}
+
+function setupBackwardPlay(repeating) {
+  
 }
 
 
