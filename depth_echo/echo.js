e47ff66fe4aa9d71ae5d3a58d308cf14a180e79e
rachel-sedibridge <rachelbridge.sedivy@gmail.com>
2025-11-14 18:39:58 -0500

    start up the folder for the echo prototype

---
+++ depth_echo/echo.js
@@ -0,0 +1,10 @@
+// JS FOR THE DEPTH-MAP ECHO IDEA
+
+/*
+IDEA:
+Like the depth muffle idea, but the first tone is always clear and just has
+a series of fading echoes indicating distance.
+
+ASSUMPTIONS:
+- 
+*/
