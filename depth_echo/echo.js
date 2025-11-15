da8f2be2bcc3f8915dfb019deda6758188600465
rachel-sedibridge <rachelbridge.sedivy@gmail.com>
2025-11-15 00:25:30 -0500

    figured out what I'm doing, started generating the sound+echo from the json

--- depth_echo/echo.js
+++ depth_echo/echo.js
@@ -13,24 +13,102 @@ ASSUMPTIONS:
 // reference global vars: these are just defaults, and are editable
 const TOGGLE_PLAY = ' '; //key to toggle play/pause
 var INIT = true; //set to false after first key pressed
+var full_tones = []; //popular during loading
+
+//TEMPLATE
+const basicTone = new Tone.Sampler({
+  urls: {
+      D1: "clean_d_str_pick-short.mp3",
+  },
+  baseUrl: "audio_tracks/",
+  release: 0.3,
+}).toDestination();
+const delays = [
+  new Tone.Delay(0.6, 5),
+  new Tone.Delay(1.3, 5),
+  new Tone.Delay(1.3, 5),
+  new Tone.Delay(3, 5)
+];
+const vols = [
+  new Tone.Volume(-8),
+  new Tone.Volume(-14),
+  new Tone.Volume(-20),
+  new Tone.Volume(-25)
+]
+// const delay_echo1 = new Tone.Delay(0.6, 5);
+// const delay_echo2 = new Tone.Delay(1.3, 5);
+// const delay_echo3 = new Tone.Delay(1.3, 5);
+// const delay_echo4 = new Tone.Delay(3, 5);
+// const vol_echo1 = new Tone.Volume(-8);
+// const vol_echo2 = new Tone.Volume(-14);
+// const vol_echo3 = new Tone.Volume(-20);
+// const vol_echo4 = new Tone.Volume(-25);
 
 
 // SETUP OF TONES
-const basicTone = new Tone.Sampler({
-    urls: {
-        D1: "clean_d_str_pick-short.mp3",
-    },
-    baseUrl: "audio_tracks/",
-    release: 0.2,
-    // onload: () => {
-    //     basicTone.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
-    // },
-}).toDestination();
-
 // run through DATA and create the echoes
 function initEchoes(data) {
   for (const [index, obj] of Object.entries(data)) {
     console.log(index, obj)
+    var name = `${obj.type}${obj.ID}`;
+    var x = obj.centroid[0];
+    var depth = obj.depth;
+    console.log(normalizeDepthToEchoes(depth))
+    var newTone = new Tone.Sampler({
+      urls: {
+          D1: "clean_d_str_pick-short.mp3",
+      },
+      baseUrl: "audio_tracks/",
+      release: 0.3,
+    });
+    // pan using x coordinate
+    var panner = new Tone.Panner(normalizePanX(x)).toDestination();
+    newTone.connect(panner);
+    // add echoes in order from nearest to farthest, stopping when dictated by `depth`
+    var numEchoes = normalizeDepthToEchoes(depth);
+    for (var i = 0; i < numEchoes; i++) {
+      newTone.chain(delays[i], vols[i], Tone.Destination);
+    }
+  }
+}
+
+// // use an array of objects as long as the object has a "time" attribute
+// const part = new Tone.Part(((time, value) => {
+//     // the value is an object which contains both the note and the velocity
+//     basicTone.triggerAttackRelease(value.note, "8n", time, value.velocity);
+// }), [{ time: 0, note: "C3", velocity: 0.9 },
+//     { time: "0:2", note: "C4", velocity: 0.5 }
+// ]).start(0);
+// Tone.Transport.start();
+
+// callback for Tone.Part
+function buildEcho(time, obj) {
+  var myTone = basicTone;
+  const delay = new Tone.Delay(obj.offset).connect
+}
+
+// normalize from x in [0, 1] to Tone.Panner input in [-1, 1]
+function normalizePanX(x) {
+  return -1 + 2 * x
+}
+
+// get the number of echoes from the depth number
+// divide into 4 "quadrants" w/ the nearest 0.05 reserved for "extreme foreground" (no echo)
+function normalizeDepthToEchoes(depth) {
+  if (depth <= 0.24) { //[0, 0.24]
+    return 4;
+  }
+  else if (depth <= 0.48) { //(0.24, 0.48]
+    return 3;
+  }
+  else if (depth <= 0.72) { //(0.48, 0.72]
+    return 2;
+  }
+  else if (depth <= 0.95) { //(0.72, 0.95]
+    return 1;
+  }
+  else { //(0.95, 1] *extreme* foreground
+    return 0;
   }
 }
 
@@ -59,5 +137,19 @@ function handleUp(e) {
 
 // helper to play basic tone
 function playTone() {
+  const freeverb = new Tone.Freeverb().toDestination();
+  freeverb.dampening = 600;
+  // routing synth through the reverb
+  // const synth = new Tone.NoiseSynth().connect(freeverb);
+  basicTone.connect(freeverb);
   basicTone.triggerAttackRelease("D1", 0.5);
+
+  // connect the signal to both the delay and the destination
+  // delay_echo1.chain(vol_echo1, Tone.Destination);
+  // basicTone.chain(delay_echo1, vol_echo1, Tone.Destination);
+  // start and stop the pulse
+  // basicTone.triggerAttackRelease("D1", 0.9);
+
+  // const vol1 = new Tone.Volume(-20).toDestination();
+  // const osc = new Tone.Oscillator().connect(vol1).start(0);
 }
