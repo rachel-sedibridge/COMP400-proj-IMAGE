dd6cc5db6e8a7cc27a58d57c7896d3f6d189aa85
rachel-sedibridge <rachelbridge.sedivy@gmail.com>
2025-11-14 20:59:11 -0500

    basic setup, plays single tone once

--- depth_echo/echo.js
+++ depth_echo/echo.js
@@ -8,3 +8,41 @@ a series of fading echoes indicating distance.
 ASSUMPTIONS:
 - 
 */
+
+// FILE-GLOBAL VARS
+// reference global vars: these are just defaults, and are editable
+const TOGGLE_PLAY = ' '; //key to toggle play/pause
+
+
+// SETUP OF TONES
+const basicTone = new Tone.Sampler({
+    urls: {
+        D1: "clean_d_str_pick-short.mp3",
+    },
+    baseUrl: "audio_tracks/",
+    release: 0.2,
+    // onload: () => {
+    //     basicTone.triggerAttackRelease(["C1", "E1", "G1", "B1"], 0.5);
+    // },
+}).toDestination();
+
+
+// KEYBINDINGS
+document.addEventListener('keydown', handleDown);
+document.addEventListener('keyup', handleUp);
+
+function handleDown(e) {
+  if (e.key != TOGGLE_PLAY) {
+    return;
+  }
+  playTone();
+}
+
+function handleUp(e) {
+  return;
+}
+
+// helper to play basic tone
+function playTone() {
+  basicTone.triggerAttackRelease("D1", 0.5);
+}
