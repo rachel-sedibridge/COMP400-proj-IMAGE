651e044d0c86d15e7cd9528904f1bcbe1ebc477b
rachel-sedibridge <rachelbridge.sedivy@gmail.com>
2025-11-18 19:42:34 -0500

    update comments & clean up

--- depth_echo/echo_time.js
+++ depth_echo/echo_time.js
@@ -1,20 +1,14 @@
-// JS FOR THE DEPTH-MAP ECHO IDEA
+// JS FOR THE DEPTH-MAP ECHO IDEA - ONE ECHO W/ MORE DISTACE = MORE DELAY
 
 /*
 IDEA:
-Like the depth muffle idea, but the first tone is always clear and just has
-a series of fading echoes indicating distance.
-
-ASSUMPTIONS:
-- 
+Like the depth muffle idea, but the first tone is always clear and just
+has an echo. The echo takes longer to sound and sounds "farther", the
+greater the depth of the object.
 
 USEFUL NOTES:
 - map interval [a,b] -> [c,d] : f(t) = c + (d-c/b-a) * (t - a)
   https://math.stackexchange.com/questions/914823/shift-numbers-into-a-different-range
-- FROM ONYX - how to make something sound far away: lowpass filter and
-  then also add reverb w/ lowpass on the reverb. Add a send to reverb,
-  have it go to main out and also to reverb, and then put a lowpass
-  filter as an insert on the reverb.
 */
 
 // FILE-GLOBAL VARS
@@ -23,24 +17,14 @@ const D_URL = 'clean_d_str_pick.mp3';
 // const schema_url = "json_schemas/city_street.json"
 const schema_url = "json_schemas/test.json"
 
+// keybinds
 const TOGGLE_PLAY = ' '; //key to toggle play/pause
 
 // sonification timing parameters
-const MAX_DELAY = 5 //in seconds, parameter for Tone.Delay objs, >delays norm to this
 const TONE_SPACING = 0.5; //in seconds, shouldn't be less than max delay
 const ECHO_DURATION = 0.7; //in seconds, how long the echoes last
 
 var toneEvents = []; //list of tone event objs used in playback, populate during loading
-
-
-//TEMPLATE
-const basicTone = new Tone.Sampler({
-  urls: {
-      D1: D_URL, //I have no idea which D it is, not 1 but doesn't matter! :D
-  },
-  baseUrl: "audio_tracks/",
-  release: 0.3,
-}).toDestination();
 
 
 // GET DATA ON OBJECTS (JSON)
@@ -49,15 +33,16 @@ const DATA = await response.json(); //json blob giving the object data
 
 
 // SETUP OF TONES
-// run through DATA (from json_loader.js) and create the tone for each detected obj
+// run through object data and create the tone + echo for each one
 for (const [index, obj] of Object.entries(DATA)) {
   var objName = `${obj.type}${obj.ID}`;
   var x = obj.centroid[0];
   var depth = obj.depth;
-  // create the main tone sampler and echo sampler
+  // create the main tone and echo samplers: diff objs bc diff effects on them
+  // NOTE: the main tone is identical for all: unique objs bc different panning
   var newTone = new Tone.Sampler({
     urls: {
-        D1: D_URL,
+        D1: D_URL, //not actually octave 1 but doesn't matter for this
     },
     baseUrl: "audio_tracks/",
     release: 0.3,
@@ -69,30 +54,31 @@ for (const [index, obj] of Object.entries(DATA)) {
     baseUrl: "audio_tracks/",
     release: 0.5, //longer release for the echo
   });
-  // pan using x coordinate
+  // set 2D pan using x coordinate of centroid
   var panner = new Tone.Panner(normalizePanX(x));
-  // newTone.connect(panner);
-  // create parts of the echo
-  var delayTime = normalizeDepthToDelay(depth);
-  var delay = new Tone.Delay(delayTime, MAX_DELAY);
+  // find the time between the intial tone and the echo
+  var delayTime = normalizeDepthToDelay(depth)
+  // create effects for the echo
   var r_decay, r_wet = normalizeDepthToReverb(depth);
-  var reverb = new Tone.Reverb({decay: r_decay, wet: r_wet});
-  var lp_freq = normalizeDepthToFilter(depth);
-  // console.log(objName, depth, lp_freq)
-  var lowPassFilter = new Tone.Filter({type: "lowpass", frequency: lp_freq});
-  // chain those parts to the echo and output, and main tone as applicable
-  // two branches of the delayed signal: one directly to main w/ volume
-  // lowered, and one goes through reverb and lp filter first
+  var reverb = new Tone.Reverb({
+    decay: r_decay,
+    wet: r_wet
+  });
+  var lowPassFilter = new Tone.Filter({
+    type: "lowpass",
+     frequency: normalizeDepthToFilter(depth)
+  });
+  // apply those effects to the echo, and the panning to both
   newTone.chain(panner, Tone.Destination);
   echo.chain(reverb, lowPassFilter, panner, Tone.Destination)
 
-  // save the final tone by name
+  // save essential info in an event array that can be passed to Part or Sequence
   toneEvents.push({
     name: objName,
     tone: newTone,
     echo: echo,
     echoDelay: delayTime,
-    time: 0 //set later! well, next thing...
+    time: 0 //set later! as in, literally the next thing...
   });
 }
 
@@ -104,44 +90,40 @@ for (var i = 0; i < toneEvents.length; i++) {
   toneEvents[i].time = curTime;
   curTime = curTime + toneEvents[i].echoDelay + ECHO_DURATION + TONE_SPACING;
 }
-console.log(toneEvents)
 
-// normalize from x in [0, 1] to Tone.Panner input in [-1, 1]
+// normalize from centroid x coord on [0, 1] to Tone.Panner input on [-1, 1]
 function normalizePanX(x) {
   // function for this in the comment block at the top of the file
   return -1 + 2 * x;
 }
 
-// get echo delay time in seconds, from depth num in json
+// get echo delay time in seconds, from obj depth num [0,1]
 function normalizeDepthToDelay(depth) {
   // [0,1] -> [c,d] : f(t) = c + (d-c/1-0) * (t - 0)
-  var delay_min = 3; //s when depth = 0
-  var delay_max = 0.01; //s when depth = 1
+  var delay_min = 3; //seconds when depth = 0
+  var delay_max = 0.01; //seconds when depth = 1
   return delay_min + (delay_max - delay_min) * depth;
 }
 
-// get the params for reverb from depth num [0,1]
-// we want: decay (s), wet (% as decimal)
+// get the params for reverb (`decay`, `wet`) from obj depth num [0,1]
 function normalizeDepthToReverb(depth) {
-  var decay_min = 5; //s when depth = 0
-  var decay_max = 0.5; //s when depth = 1
+  var decay_min = 5; //seconds, when depth = 0
+  var decay_max = 0.5; //seconds, when depth = 1
   var decay = decay_min + (decay_max - decay_min) * depth;
 
-  var wet_min = 0.96; //%wet when depth = 0
-  var wet_max = 0.5; //%wet when depth = 1
+  var wet_min = 0.96; //wet-dry balance when depth = 0
+  var wet_max = 0.5; //wet-dry balance when depth = 1
   var wet = wet_min + (wet_max - wet_min) * depth;
 
   return decay, wet;
 }
 
-// get the params for lowpass filter from depth num [0,1]
-// we want: high (decibels) = amount high frequency range is suppressed,
-// highFrequency (Hz) = mid/high cutoff freq., i.e. where to start limiting
+// get the cutoff frequency of the low pass filter from obj depth num [0,1]
 function normalizeDepthToFilter(depth) {
   // I went on desmos until I got a func that looked the right shape... see docs
   var freq_min = 950; //Hz when depth = 0
   var freq = freq_min + Math.pow((9 * depth + 0.5), 3.7);
-  // puts max cutoff at ~5100, want no discernible EQ on extremely close objs
+  // ^sets max cutoff ~5100, but want no discernible EQ on extremely close objs
   if (depth > 0.95) {
     freq = 6000
   }
@@ -159,7 +141,9 @@ function handleDown(e) {
   }
   if (Tone.getTransport().state == "started") {
     Tone.getTransport().toggle();
-    // NOTE: this doesn't stop the playback once it's started, but it lets you play it again.
+    // NOTE: this doesn't stop the playback once it's started, but
+    // it at least lets you play it again.
+    // TODO: make it stop playback once it's started
   }
   else {
     playAllTones();
@@ -180,45 +164,25 @@ function playAllTones() {
 // the callback for the Tone.Part that plays all the tones
 // args MUST be (time, value) (API requirement)
 function playTone(time, value) {
-  // always the same note, time comes from ^Sequence.subdivision^
   // value contains `name` for the 'captioning', not implemented yet
   var duration = value.echoDelay < 0.4 ? 0.4 : value.echoDelay;
-  console.log(duration)
   value.tone.triggerAttackRelease("D1", duration, time);
   value.echo.triggerAttackRelease("D1", ECHO_DURATION, time + value.echoDelay);
 }
 
-// moved all the messing around from playTone() to here
-// because the name `playTone()` made more sense to use elsewhere lol
+
+// holds my experimental messing around, you may disregard :)
 function tester() {
+  const basicTone = new Tone.Sampler({
+    urls: {
+        D1: D_URL,
+    },
+    baseUrl: "audio_tracks/",
+    release: 0.3,
+  }).toDestination();
   // const freeverb = new Tone.Freeverb().toDestination();
   // freeverb.dampening = 1000;
   // basicTone.connect(freeverb);
-  // basicTone.triggerAttackRelease("D1", 0.9);
-
-  // connect the signal to both the delay and the destination
-  // delay_echo1.chain(vol_echo1, Tone.Destination);
-  // basicTone.chain(delays[0], vols[0], Tone.Destination);
-  // basicTone.chain(delays[1], vols[1], Tone.Destination);
-  // basicTone.chain(delays[2], vols[2], Tone.Destination);
-  // start and stop the pulse
-  // basicTone.triggerAttackRelease("D1", 0.9);
-
-  // const delay1 = new Tone.Delay(normalizeDepthToDelay(1), MAX_DELAY)
-  // const delay2 = new Tone.Delay(normalizeDepthToDelay(0.75), MAX_DELAY)
-  // const delay3 = new Tone.Delay(normalizeDepthToDelay(0.5), MAX_DELAY)
-  // const delay4 = new Tone.Delay(normalizeDepthToDelay(0.25), MAX_DELAY)
-  // const delay5 = new Tone.Delay(normalizeDepthToDelay(0), MAX_DELAY)
-  // console.log(normalizeDepthToDelay(0))
-  // console.log(normalizeDepthToDelay(0.25))
-  // console.log(normalizeDepthToDelay(0.5))
-  // console.log(normalizeDepthToDelay(0.75))
-  // console.log(normalizeDepthToDelay(1))
-  // basicTone.chain(delay1, Tone.Destination)
-  // basicTone.chain(delay2, Tone.Destination)
-  // basicTone.chain(delay3, Tone.Destination)
-  // basicTone.chain(delay4, Tone.Destination)
-  // basicTone.chain(delay5, Tone.Destination)
   // basicTone.triggerAttackRelease("D1", 0.9);
 
   // const vol1 = new Tone.Volume(-20).toDestination();
